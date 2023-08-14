b6885fbfdf24718a79308adf0af06af246931602
Emil57 <trejocovantesde@gmail.com>
2023-08-14 14:25:07 -0700

    agregando DAO y categorias

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -6,21 +6,27 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.latam.alura.tienda.dao.CategoriaDAO;
+import com.latam.alura.tienda.dao.ProductoDAO;
+import com.latam.alura.tienda.modelo.Categoria;
 import com.latam.alura.tienda.modelo.Producto;
+import com.latam.alura.tienda.utils.JPAUtils;
 
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
-
-		Producto celular = new Producto();
-		celular.setNombre("Samsung");
-		celular.setDescripcion("Cel usado");
-		celular.setPrecio(new BigDecimal(1000));
+		Categoria celulares= new Categoria("Celulares");
+		Producto celular = new Producto("Samsung", "celular usado",new BigDecimal(1000), celulares);
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();;
-		em.persist(celular);
+		EntityManager em = JPAUtils.getEntityManager();
+		
+		ProductoDAO productoDAO = new ProductoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		
+		em.getTransaction().begin();
+		
+		categoriaDAO.guardar(celulares);
+		productoDAO.guardar(celular);
 		em.getTransaction().commit();
 		em.close();
 	}
